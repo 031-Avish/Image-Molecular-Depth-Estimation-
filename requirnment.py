82081a84561e01596ddc79e1e32a8ae1892079b1
031-Avish <93810970+031-Avish@users.noreply.github.com>
2022-11-07 13:08:02 +0530

    Create requirnment.py

---
+++ requirnment.py
@@ -0,0 +1,12 @@
+import os
+import sys
+
+import tensorflow as tf
+from tensorflow.keras import layers
+
+import pandas as pd
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+
+tf.random.set_seed(123)
